4d8132d44a6583573d7d31754b862a7fb09f1c9d
fraisse <fraisse@normind.com>
2011-09-14 09:18:44 +0200

    project initialization

---
+++ js/github-tools-ui.js
@@ -0,0 +1,244 @@
+google.load("visualization", "1", {packages:["treemap"]});
+if (typeof com == "undefined") { com = {}; }
+
+(function(){
+
+	com.github = com.github || {};
+	
+	com.github.ui = {
+	
+		span: function(spanId, spanContent) {
+			var span = $('<span/>', {
+				'id': 'detail-' + spanId,
+				html: spanContent
+			});
+			return span;
+		},
+		
+		div: function(divId, divContent) {
+			var div = $('<div/>', {
+				'id': 'detail-' + divId,
+				html: divContent
+			});
+			return div;
+		},
+		
+		analysisAnchor: function(repository, anchorContent) {
+			var anchor = $('<a/>', {
+				'id': 'detail-a-analysis',
+				'class': 'button',
+				'href': 'repo.html?user=' + repository.owner + '&repo=' + repository.name,
+			    html: anchorContent
+			});
+			return anchor;
+		},
+		
+		addDetailResult: function(repository, index) {
+			var details = $("#result-details");
+			
+			var detailDiv = $('<div/>', {
+				'id': 'repo' + index,
+				'class': 'result-detail' + ((index == 0) ? ' active' : '')
+			});
+			detailDiv.append(self.span('name', repository.name));
+			detailDiv.append(' | ');
+			detailDiv.append(self.span('owner', repository.owner));
+			detailDiv.append(self.div('lang', repository.language));
+			detailDiv.append(self.div('desc', repository.description));
+			var watcherSpan = self.span('watchers', repository.watchers);
+			var forkSpan = self.span('forks', repository.forks);
+			detailDiv.append(self.div('more', 'This repository has ' + watcherSpan.text() + ' watcher(s) and ' + forkSpan.text() + ' fork(s). If you would like more information about this repository, please visite ' + repository.name + ' repository on <a href="http://github.com/' + repository.owner + '/' + repository.name + '">github</a>.'));
+			detailDiv.append(self.analysisAnchor(repository, repository.name + ' analysis'));
+			
+			details.append(detailDiv);
+		},
+		
+		addShortResult: function(repository, index) {
+			var anchor =	$('<a/>', {
+			    'href': '#repo' + index,
+				html: repository.name
+			});
+			var span = $('<div/>', {
+				'id': 'repotext',
+				html: '<b>' + repository.owner + '</b> | ' + repository.forks + ' forks | ' + repository.watchers + ' watchers' 
+			});
+			var li = $('<li/>', {
+				'class': ((index == 0) ? 'active' : 'inactive')
+			});
+			$(".list").append(li.append($('<h2/>').append(anchor)));
+			li.append(span);
+		},
+	
+		showRepository: function(repository, index) {
+			self.addShortResult(repository, index);
+			self.addDetailResult(repository, index);
+		},
+	
+		commitorLi: function(contributor) {
+			var contributorName = contributor.name;
+			var contributorLogin = contributor.login;
+			if (contributorName == null || contributorName == '') {
+				contributorName = contributorLogin;
+			}
+			var contributorType = contributor.type;
+			if (contributorType == null || contributorType == '') {
+				contributorType = 'anonymous';
+			}
+			
+			var li = $('<li/>', {
+				'class': contributor.type
+			});
+			li.append($('<img/>', {
+				'class': 'gravatar',
+				'src': 'http://www.gravatar.com/avatar/' + contributor.gravatar_id + '?s=48'
+			}));
+			
+			var loginAndName = null;
+			if (contributorType == 'anonymous') {
+				loginAndName = '<em>' + contributor.name + '</em>';
+			}
+			else {
+				loginAndName = '<a href="http://github.com/' + contributor.login + '">' + contributor.login + '</a> '
+					+ '<em>(' + contributor.name + ')</em>';
+			}
+			li.append(self.div('contributor-login', loginAndName));
+
+			if (contributor.blog != null && contributor.blog != 'undefined') {
+				var anchor =$('<a/>', {
+				    'href': contributor.blog,
+					html: contributor.blog
+				});
+				li.append(self.div('contributor-blog').append(anchor));
+			}
+			
+			if (contributor.location != null && contributor.location != 'undefined') {
+				li.append(self.div('location', contributor.location));
+				li.append(self.div('separator', ' | '));
+			}
+
+			li.append(self.div('contribution-count', '<strong>' + contributor.contributions + '</strong> commit(s)'));
+
+			return li;
+		},
+
+		showCommitor: function(contributor) {
+			$("#result-list .list").append(self.commitorLi(contributor));
+		},
+	
+		showCommittersAsTreeMap: function(commitsByUser, divId) {
+			var dataTable = new google.visualization.DataTable();
+			dataTable.addColumn('string', 'Name');
+			dataTable.addColumn('string', 'parent');
+			dataTable.addColumn('number', 'Contribution count (size)');
+	
+			dataTable.addRow(['Commiters', null, 0]);
+	
+			for (var committerIndex in commitsByUser) {
+				var committer = commitsByUser[committerIndex];
+				dataTable.addRow([committer.user, 'Commiters', committer.commitCount]);
+			}
+	
+			// Create and draw the visualization.
+			var elt = document.getElementById(divId);
+			var tree = new google.visualization.TreeMap(elt);
+			tree.draw(dataTable, {
+				minColor: '#4676af',
+				midColor: '#ddd',
+				maxColor: '#9dde74',
+				headerHeight: 15,
+				fontColor: 'black',
+				showScale: true
+			});
+		},
+
+		showCommitsOnTimeline: function(commits, containerId) {
+			var data = new Array();
+			var commitCount = 0;
+			var commitIndex = 0;
+			for (var dateIndex in commits) {
+				var commit = [new Date(dateIndex).getTime(), commits[dateIndex].commitCount];
+				data[commitIndex] = commit;
+				commitIndex++;
+				commitCount += commits[dateIndex].commitCount;
+			}
+	
+	        function weekendAreas(axes) {
+	            var markings = [];
+	            var d = new Date(axes.xaxis.min);
+	            // go to the first Saturday
+	            d.setUTCDate(d.getUTCDate() - ((d.getUTCDay() + 1) % 7))
+	            d.setUTCSeconds(0);
+	            d.setUTCMinutes(0);
+	            d.setUTCHours(0);
+	            var i = d.getTime();
+	            do {
+	                // when we don't set yaxis, the rectangle automatically
+	                // extends to infinity upwards and downwards
+	                markings.push({ xaxis: { from: i, to: i + 2 * 24 * 60 * 60 * 1000 } });
+	                i += 7 * 24 * 60 * 60 * 1000;
+	            }
+	            while (i < axes.xaxis.max);
+	    
+	            return markings;
+	        }
+	
+			var placeholderId = $('<div/>', { 'id': 'visualization' });
+	    	placeholderId.appendTo($(containerId));
+	    	
+	    	var overviewId = $('<div/>', { 'id': 'overview' });
+			overviewId.appendTo($(containerId));
+	
+			var overviewLegendId = $('<div/>', { 'id': 'overviewLegend' });
+			overviewLegendId.appendTo($(containerId));
+	
+			var options = {
+				legend: { show: true, container: overviewLegendId },
+		        bars: { show: true, fill: true, barWidth: 24 * 60 * 60 * 1000 },
+				xaxis: { mode: "time", tickLength: 5, zoomRange: [0.1, 10] },
+				selection: { mode: "x" },
+		        zoom: { interactive: true },
+	            grid: { markings: weekendAreas }
+			};
+	        var serie = { data: data, label: "last " + commitCount + " commits", color: "#aaa" };
+		    var plot = $.plot($(placeholderId), [serie], options);
+	
+	
+		    $(placeholderId).bind('plotzoom', function (event, plot) {
+		        var axes = plot.getAxes();
+		        $(".message").html("Zooming to x: "  + axes.xaxis.min.toFixed(2)
+		                           + " &ndash; " + axes.xaxis.max.toFixed(2)
+		                           + " and y: " + axes.yaxis.min.toFixed(2)
+		                           + " &ndash; " + axes.yaxis.max.toFixed(2));
+		    });
+		
+		
+	        // setup overview
+	        var overview = $.plot($(overviewId), [{ data: data, color: "#aaa" }], {
+		        bars: { show: true, fill: true },
+	            xaxis: { ticks: [], mode: "time" },
+	            yaxis: { ticks: [], min: 0, autoscaleMargin: 0.1 },
+	            selection: { mode: "x" },
+				grid: { markings: weekendAreas }
+	        });
+	
+	        // now connect the two
+	    
+	        $(placeholderId).bind("plotselected", function (event, ranges) {
+	            // do the zooming
+	            plot = $.plot($(placeholderId), [serie],
+	                          $.extend(true, {}, options, {
+	                              xaxis: { min: ranges.xaxis.from, max: ranges.xaxis.to }
+	                          }));
+	        
+	            // don't fire event on the overview to prevent eternal loop
+	            overview.setSelection(ranges, true);
+	        });
+	        $(overviewId).bind("plotselected", function (event, ranges) {
+	            plot.setSelection(ranges);
+	        });
+		},
+	};
+
+	var self = com.github.ui;
+	
+})();
---
+++ js/github-tools.js
@@ -0,0 +1,188 @@
+
+if (typeof com == "undefined") { com = {}; }
+
+(function() {
+com.github = com.github || {};
+
+// constructor
+com.github.Repository = function() {
+	var readParams = new Array();
+	var query = window.location.search.substring(1);
+	var params = query.split('&');
+	for (var i=0; i<params.length; i++) {
+		var param = params[i].split('=');
+		if (param.length > 0) {
+			var key = param[0];
+			var value = param[1];
+			readParams[key] = value;
+		}
+	}
+    this.name = readParams['repo'];
+    this.owner = readParams['user'];
+    
+    com.github.utils.loadCommits(this);
+};
+
+com.github.Repository.prototype = {
+	name: null,
+    owner: null,
+    nethash: null,
+    contributors: null,
+    Commits: {
+    	rowData: null,
+    	commitsByUser: null,
+    	commitsByDate: null,
+    },
+
+
+	showContributors: function(displayContributor, postFunction) {
+		var url = 'http://github.com/api/v2/json/repos/show/'
+				 + this.owner
+				 + '/'
+				 + this.name
+				 + '/contributors/anon';
+		$.ajax({
+			url: url,
+			dataType: 'jsonp',
+			success: function(data) {
+				var contributors = data.contributors;
+				contributors.sort(function(firstContributor, secondContributor) {
+					return secondContributor.contributions - firstContributor.contributions;
+				});
+				
+				$.each(contributors, function(i, contributor) {
+					// Some User does not have defined name, set the login as name
+					if (contributor.name == null || contributor.name == '') {
+						contributor.name = contributor.login;
+					}
+					if (contributor.type == null || contributor.type == '') {
+						contributor.type = 'anonymous';
+					}
+					displayContributor(contributor, i);
+				});
+				postFunction();
+			}
+		});
+	},
+
+
+	sortCommitsByContributionCount: function(commits) {
+		commits.sort(function(firstCommit, secondCommit) {
+			return firstCommit.commitCount - secondCommit.commitCount;
+		});
+	},
+
+	newCommit: function(currentData) {
+		var login = currentData.login;
+		if (login == '') { login = currentData.author; }
+		var commit = {
+			user: currentData.author,
+			login: login,
+			commitCount: 1,
+			date: currentData.date,
+			gravatar: currentData.gravatar
+		};
+		return commit;
+	},
+	
+	/**
+	 * Returns and caches all the contributors based on the last 100 commits.
+	 */
+	getCommitsByUser: function() {
+		var	commits = this.Commits.commitsByUser;
+		if (commits == null) {
+			commits = new Array();
+			var rowData = this.Commits.rowData;
+			var commitCount = Math.min(MAX_COMMIT_COUNT, rowData.length);
+			for (var i = 0; i < commitCount; i++) {
+				var currentData = rowData[i];
+				var commit = commits[currentData.author];
+				if (commit == null) {
+					commits[currentData.author] = this.newCommit(currentData);
+				}
+				else { commit.commitCount++; }
+			}
+			
+			// And caches the result
+			this.Commits.commitsByUser = commits;
+		}
+		return commits;
+	},
+
+	/**
+	  * Returns all the commits, indexed by dates, based on the last 100 commits.
+	  */
+	getCommitsByDate: function() {
+		var	commits = this.Commits.commitsByDate;
+		if (commits == null) {
+			commits = new Array();
+			var rowData = this.Commits.rowData;
+			var commitCount = Math.min(MAX_COMMIT_COUNT, rowData.length);
+			for (var i = 0; i < commitCount; i++) {
+				var currentData = rowData[i];
+				var dateAsDay = currentData.date.substring(0, 10);
+				var commit = commits[dateAsDay];
+				if (commit == null) {
+					commits[dateAsDay] = this.newCommit(currentData);
+				}
+				else { commit.commitCount++; }
+			}
+			this.Commits.commitsByDate = commits;
+		}
+		return commits;
+	}
+
+};
+
+var self = com.github.Repository,
+	MAX_COMMIT_COUNT = 100;
+
+com.github.utils = {
+    searchRepository: function(displayRepository, postFunction) {
+        var query = document.getElementById('searchText').value;
+        var url = 'http://github.com/api/v2/json/repos/search/' + query;
+
+        $.ajax({
+            url: url,
+            dataType: 'jsonp',
+            success: function(data) {
+                $.each(data.repositories, function(i, repository) {
+                	displayRepository(repository, i);
+                });
+                postFunction();
+            }
+        });
+    },
+		
+	loadCommits: function(repository) {
+		var url = 'http://github.com/' + repository.owner + '/' + repository.name + '/network_meta';
+		$.ajax({
+			url: url,
+			dataType: 'jsonp',
+			success: function(data) {
+				repository.nethash = data.nethash;
+				com.github.utils.loadCommitsFromNethash(repository);
+			}
+		});
+	},
+			
+	loadCommitsFromNethash: function(repository) {
+		var url = 'http://github.com/' + repository.owner + '/' + repository.name + '/network_data_chunk?nethash=' + repository.nethash;
+		$.ajax({
+			url: url,
+			dataType: 'jsonp',
+			success: function(data) {
+				data.commits.sort(function(firstCommit, secondCommit) {
+					var comparison = null;
+					if (firstCommit.date < secondCommit.date) { comparison = 1; }
+					else if (firstCommit.date == secondCommit.date) { comparison = 0; }
+					else { comparison = -1; }
+					return comparison;
+				});
+				repository.Commits.rowData = data.commits;
+			}
+		});
+	}
+}
+
+})();
